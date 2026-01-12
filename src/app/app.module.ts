95a05943d3cb8c4a93f7cfbba937219bd231f7c0
pratikshakulkarni2000 <pratikshak2404@gmail.com>
2026-01-12 17:03:26 +0530

    Read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoComponent } from './shared/components/todo/todo.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/consts/todo.ts
@@ -0,0 +1,16 @@
+import { Itodo } from "../models/todo";
+
+export const TodoData : Array<Itodo> = [
+    {
+        todoItem : 'Angular',
+        todoId : '1'
+    },
+     {
+        todoItem : 'JavaScript',
+        todoId : '11'
+    },
+     {
+        todoItem : 'Flex',
+        todoId : '111'
+    }
+]
