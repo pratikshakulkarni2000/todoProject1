478c95d458b152a1625ba2db0ab3589c50ed756f
dhanashri-bhopale <bhopaledhanashri@gmail.com>
2026-01-14 15:35:48 +0530

    create remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TodoComponent } from './shared/components/todo/todo.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { GetConfirmComponent } from './shared/components/get-confirm/get-confirm.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    GetConfirmComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSnackBarModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/todo/todo.component.ts
+++ src/app/shared/components/todo/todo.component.ts
@@ -1,26 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TodoData } from '../../consts/todo';
 import { Itodo } from '../../models/todo';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { GetConfirmComponent } from '../get-confirm/get-confirm.component';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todoArr: Array<Itodo> = TodoData;
 
-  todoArr : Array<Itodo> = TodoData
+  isInEditMode: boolean = false;
 
-  isInEditMode : boolean = false
+  @ViewChild('todoItem') todoItem!: ElementRef;
 
-  trackById (index : number,todo : Itodo){
-    return todo.todoId
+  trackById(index: number, todo: Itodo) {
+    return todo.todoId;
   }
 
+  constructor(
+    private _snackBar: MatSnackBar,
 
-  constructor() { }
+    private _matDialog: MatDialog
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddTodo() {
+    if (this.todoItem.nativeElement.value.length > 0) {
+      let todoObj: Itodo = {
+        todoItem: this.todoItem.nativeElement.value,
+        todoId: Date.now().toString(),
+      };
+      console.log(todoObj);
+      this.todoArr.unshift(todoObj);
+      this.todoItem.nativeElement.value = '';
+
+      this._snackBar.open(`TodoItem Added Successfully!!!`, 'Close', {
+        horizontalPosition: 'left',
+        verticalPosition: 'bottom',
+        duration: 3000,
+      });
+    }
   }
 
+  onRemove(id: string) {
+    let matConfig = new MatDialogConfig();
+    matConfig.disableClose = true;
+
+    let matDialogRef = this._matDialog.open(GetConfirmComponent, matConfig);
+    matDialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        let getIndex = this.todoArr.findIndex((t) => t.todoId === id);
+        this.todoArr.splice(getIndex, 1);
+
+        this._snackBar.open(`The TodoItem Removed Successflly`,'Close', {
+          horizontalPosition: 'left',
+          verticalPosition: 'bottom',
+          duration: 3000
+        })
+      }
+    });
+  }
 }
